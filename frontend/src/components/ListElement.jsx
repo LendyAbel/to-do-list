66b76128ab40423050d4cb55e7890d21c8252488
LendyAbel <lendyabel93@gmail.com>
2025-07-03 10:30:51 +0200

    ui/Somo style for the list

--- frontend/src/components/ListElement.jsx
+++ frontend/src/components/ListElement.jsx
@@ -1,14 +1,14 @@
 const ListElement = ({ element }) => {
   const { title, description, date } = element
   return (
-    <div>
-      <input type="checkbox" />
-      <div>
+    <div className="grid grid-cols-[auto_1fr] items-center gap-5 rounded-lg bg-white p-5 shadow-md">
+      <input className="scale-200" type="checkbox" />
+      <div className="flex w-full flex-col gap-2">
         <div>
-          <h3>{title}</h3>
-          <p>{date}</p>
+          <h3 className="text-2xl font-bold">{title}</h3>
+          <p>{description}</p>
         </div>
-        <p>{description}</p>
+        <p className="text-right">{date}</p>
       </div>
     </div>
   )
--- frontend/src/components/ToDoList.jsx
+++ frontend/src/components/ToDoList.jsx
@@ -6,8 +6,7 @@ console.log(list)
 
 const ToDoList = () => {
   return (
-    <div className="dar bg-white">
-      <h2>List of task:</h2>
+    <div className="mx-auto flex w-5/6 flex-col gap-3 rounded-lg bg-yellow-200 p-3 shadow-lg">
       {list.map((element) => {
         return <ListElement key={element.id} element={element} />
       })}
