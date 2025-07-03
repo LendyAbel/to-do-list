c89a394f3d64c96ddec175c20aeac0906e81c7e2
LendyAbel <lendyabel93@gmail.com>
2025-07-03 13:58:44 +0200

    ui/Adding motion to animation

--- frontend/src/components/ListElement.jsx
+++ frontend/src/components/ListElement.jsx
@@ -1,14 +1,53 @@
+import { useRef, useState, useEffect } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+
 const ListElement = ({ element }) => {
+  const [expand, setExpand] = useState(false)
+  const [height, setHeight] = useState(0)
+  const ref = useRef(null)
+
   const { title, description, date } = element
+
+  useEffect(() => {
+    if (expand && ref.current) {
+      console.log(ref.current.scrollHeight);
+      
+      setHeight(ref.current.scrollHeight)
+    }
+  }, [expand, description])
+
+  const handleExpand = () => setExpand((prev) => !prev)
+
   return (
-    <div className="grid grid-cols-[auto_1fr] items-center gap-5 rounded-lg bg-white p-5 shadow-md">
-      <input className="scale-200" type="checkbox" />
+    <div
+      onClick={handleExpand}
+      className="grid cursor-pointer grid-cols-[auto_1fr] items-center gap-5 rounded-lg bg-white p-5 shadow-md"
+    >
+      <input
+        className="scale-200"
+        type="checkbox"
+        onClick={(e) => e.stopPropagation()}
+      />
       <div className="flex w-full flex-col gap-2">
         <div>
           <h3 className="text-2xl font-bold">{title}</h3>
-          <p>{description}</p>
+          <AnimatePresence initial={false}>
+            {expand && (
+              <motion.div
+                className="overflow-hidden"
+                initial={{ height: 0 }}
+                animate={{ height }}
+                exit={{ height: 0 }}
+                transition={{ duration: 0.2 }}
+              >
+                <div ref={ref}>
+                  <p>{description}</p>
+                  <p className="text-right">{date}</p>
+                </div>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
-        <p className="text-right">{date}</p>
       </div>
     </div>
   )
--- frontend/src/components/ToDoList.jsx
+++ frontend/src/components/ToDoList.jsx
@@ -2,8 +2,6 @@ import ListElement from './ListElement'
 
 import list from '../data.json'
 
-console.log(list)
-
 const ToDoList = () => {
   return (
     <div className="mx-auto flex w-5/6 flex-col gap-3 rounded-lg bg-yellow-200 p-3 shadow-lg">
