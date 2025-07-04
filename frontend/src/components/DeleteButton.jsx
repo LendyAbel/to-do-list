596f270ea665492624fe8064978a017cbb3a4562
LendyAbel <lendyabel93@gmail.com>
2025-07-04 14:02:05 +0200

    feat/Working on delete button

--- frontend/src/components/DeleteButton.jsx
+++ frontend/src/components/DeleteButton.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
 import deleteIcon from '../assets/icons/icons8-basura-llena-48.svg'
 
-const DeleteButton = () => {
+const DeleteButton = ({deleteElement}) => {
+
+handleClick = (e) => {
+  e.stopPropagation()
+  deleteElement()
+}
+
   return (
     <div className="flex w-full flex-row justify-end">
       <button
         className="flex w-fit cursor-pointer items-center gap-2 rounded-lg bg-red-300 px-3 py-2 hover:bg-red-400"
-        onClick={(e) => {
-          e.stopPropagation()
-        }}
+        onClick={handleClick}
       >
         <img src={deleteIcon} alt="delete icon" className="w-5" />
       </button>
