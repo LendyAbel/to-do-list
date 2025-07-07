80d8bb8558edac91c6b4b847f7cd591d24a9940d
LendyAbel <lendyabel93@gmail.com>
2025-07-07 17:15:25 +0200

    fix/Changing somo animations

--- frontend/DevLog.md
+++ frontend/DevLog.md
@@ -1,2 +1,4 @@
 npm install tailwindcss @tailwindcss/vite
 npm install -D prettier prettier-plugin-tailwind
+
+improve filter, adding states to notes elements
--- frontend/src/components/ToDoList.jsx
+++ frontend/src/components/ToDoList.jsx
@@ -65,23 +65,23 @@ const ToDoList = () => {
   }
 
   return (
-    <div className="mx-auto flex w-[96%] flex-col gap-3 rounded-lg bg-yellow-200 p-3 shadow-lg">
+    <div className="mx-auto flex min-h-screen w-[96%] flex-col gap-3 rounded-lg bg-yellow-200 p-3 shadow-lg">
       {formOpen ? (
         <AddForm handleClose={closeForm} handleAdd={handleAddForm} />
       ) : (
         <>
           <AddButton openForm={openForm} />
           <Filter showToDo={showToDo} showDone={showDone} showAll={showAll} />
-          <AnimatePresence>
+          <AnimatePresence >
             {listToShow.map((element) => {
               return (
                 <motion.div
                   key={element.id}
                   layout
-                  initial={{ opacity: 0, y: 20 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: 20 }}
-                  transition={{ type: 'spring', stiffness: 200, damping: 20 }}
+                  initial={{ opacity: 0, scale: 0.8 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  exit={{ opacity: 0, scale: 0.8 }}
+                  transition={{ duration: 0.2 }}
                 >
                   <Element
                     key={element.id}
