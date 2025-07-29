abe7f998cce66e26518ce63f1f132f8ee61592c4
LendyAbel <lendyabel93@gmail.com>
2025-07-29 11:06:13 +0200

    [feat] Adding get request and improving console.logs

--- backend/controllers/list.js
+++ backend/controllers/list.js
@@ -7,8 +7,25 @@ const { generateId } = require('../utils/list_helper')
 const dbPath = path.join(__dirname, '../db/db.json')
 const listDB = JSONFilePreset(dbPath, { list: [] })
 
+listRouter.get('/', async (req, res) => {
+  console.log('----------------')
+  console.log('Recived request to get list')
+  console.log('----------------')
+
+  const db = await listDB
+  const { list } = db.data
+  res
+    .status(200)
+    .json(list)
+    .end(() => {
+      console.log('List sent successfully')
+    })
+})
+
 listRouter.post('/', async (req, res) => {
+  console.log('----------------')
   console.log('Recived request to add item:', req.body)
+  console.log('----------------')
 
   const { title, description } = req.body
 
@@ -20,7 +37,12 @@ listRouter.post('/', async (req, res) => {
   }
   const db = await listDB
   await db.update(({ list }) => list.push(newElement))
-  res.status(201).json(newElement)
+  res
+    .status(201)
+    .json(newElement)
+    .end(() => {
+      console.log('Item added successfully:', newElement)
+    })
 })
 
 module.exports = listRouter
