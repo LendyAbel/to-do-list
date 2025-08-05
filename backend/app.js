a968ea9963a3f61975fd31d49c4f3cf0ff4b2bc8
LendyAbel <lendyabel93@gmail.com>
2025-08-05 16:46:38 +0200

    [fix] Fixing when you reload page lose the conexion to the route

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
-
+const path = require('path')
+const fs = require('fs')
 
 const listRouter = require('./controllers/list')
 const userRouter = require('./controllers/user')
@@ -17,5 +18,9 @@ app.use(express.json())
 app.use('/api/list', authMiddleware, listRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+})
 
 module.exports = app
